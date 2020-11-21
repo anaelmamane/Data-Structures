1ec455c1f1d6f19705c727070a34493c0f09c51e
enemiasufl <58480376+enemiasufl@users.noreply.github.com>
2020-11-21 13:25:58 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Case{
+	string sex; //Holds the sex of the person
+	string age; //Holds the age range of the person
+	string dateCDC; //Holds the data the CDC report data
+	string race; //Holds the race person
+	char hospitalization; //Hospitalization
+	char icu; //ICU admittance
+	char death; //Did the person die
+	char medicalCondition; //Did the person have a medical condition
+};
 
 int main() {
 	cout << "Hello World" << endl;
 	cout << "Help " << endl;
-	int i;
-	i=3;
-	i++;
 }
